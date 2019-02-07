64d974ab27d7124c446ec71ece3953d778a481f7
q138ben <binbins@kth.se>
2019-02-07 16:50:42 +0100

    Delete autolabel, put the bar in the middle

--- barchart_accuracy_p5_CNN.py
+++ barchart_accuracy_p5_CNN.py
@@ -22,8 +22,7 @@ def bar_accuracy(CNN):
     width = 0.35  # the width of the bars
     
     fig, ax = plt.subplots()
-    rects1 = ax.bar(ind - width/2, CNN, width, 
-                    color='SkyBlue', label='CNN')
+    ax.bar(ind, CNN, width, color='SkyBlue', label='CNN')
 
     
     # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -32,30 +31,10 @@ def bar_accuracy(CNN):
     ax.set_ylim(ymin=0.7)
     ax.set_xticks(ind)
     ax.set_xticklabels(('Overall', 'LR', 'MS', 'TS', 'PSw','Sw'))
-    ax.legend(loc='lower right')
+    ax.legend(loc=0)
     
 #    ax.set_ylim(0,1.2)
     
-    
-    def autolabel(rects, xpos='center'):
-        """
-        Attach a text label above each bar in *rects*, displaying its height.
-    
-        *xpos* indicates which side to place the text w.r.t. the center of
-        the bar. It can be one of the following {'center', 'right', 'left'}.
-        """
-    
-        xpos = xpos.lower()  # normalize the case of the parameter
-        ha = {'center': 'center', 'right': 'left', 'left': 'right'}
-        offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
-    
-        for rect in rects:
-            height = rect.get_height()
-            ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,
-                    '{}'.format(height), ha=ha[xpos], va='bottom')
-    
-    
-#    autolabel(rects1, "left")
-#    autolabel(rects2, "right")
+
     
     plt.show()
