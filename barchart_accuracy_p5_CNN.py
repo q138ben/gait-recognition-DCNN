c357f11fb503690f885dcd6f019f746656ba0226
q138ben <binbins@kth.se>
2019-02-07 17:30:24 +0100

    Adjust font size on tick labels

--- barchart_accuracy_p5_CNN.py
+++ barchart_accuracy_p5_CNN.py
@@ -19,19 +19,20 @@ def bar_accuracy(CNN):
     
 
     ind = np.arange(len(CNN))  # the x locations for the groups
-    width = 0.35  # the width of the bars
+    width = 0.5  # the width of the bars
     
     fig, ax = plt.subplots()
     ax.bar(ind, CNN, width, color='SkyBlue', label='CNN')
 
     
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Acuracy')
-#    ax.set_title('Scores by group and gender')
+    ax.set_ylabel('Acuracy',fontsize=15)
+    ax.set_title('CNN recognition accuracy on 5 gait phases',fontsize=15)
     ax.set_ylim(ymin=0.7)
     ax.set_xticks(ind)
     ax.set_xticklabels(('Overall', 'LR', 'MS', 'TS', 'PSw','Sw'))
-    ax.legend(loc=0)
+    ax.tick_params(axis='both', which='major', labelsize=12)
+#    ax.legend(loc=0,fontsize=15)
     
 #    ax.set_ylim(0,1.2)
     
