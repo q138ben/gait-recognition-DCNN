cddb5cb99f6c4187861f5161261bdc454681f9ec
q138ben <binbins@kth.se>
2019-02-12 16:18:07 +0100

    Add padding to the convolutional layer, achieving 95% acuracy

--- DNM.py
+++ DNM.py
@@ -8,40 +8,52 @@ Created on Mon Feb 11 16:27:48 2019
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.layers import Dense, Embedding
+from keras.layers import Embedding
 from keras.layers import LSTM
 
+from keras.layers import Conv2D
 
 
 def CNN(X_train,Y_train):
     
     batch_size = 200
     nb_classes = 5
-    nb_epoch = 10
+    nb_epoch = 30
     
     # input image dimensions
     img_rows, img_cols = 6, 9
     # number of convolutional filters to use
-    nb_filters = 8
+    nb_filters = 16
     # size of pooling area for max pooling
-    pool_size = (2, 3)
+    pool_size = (2, 2)
     # convolution kernel size
     kernel_size = (2, 2)
     
     
     model = Sequential()
     
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='valid',
-                        input_shape=(img_rows, img_cols, 1)))
+
+
+    model.add(Conv2D(nb_filters, kernel_size,input_shape=(img_rows, img_cols, 1),padding='same'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+    model.add(Conv2D(nb_filters, kernel_size))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size))
     model.add(Dropout(0.25))
-    
+
+#    model.add(Conv2D(2*nb_filters, kernel_size,padding = 'same'))
+#    model.add(Activation('relu'))
+#    model.add(Conv2D(2*nb_filters, kernel_size,padding = 'same'))
+#    model.add(Activation('relu'))
+#    model.add(MaxPooling2D(pool_size=pool_size))
+#    model.add(Dropout(0.25))
+
+
+
+
+
     model.add(Flatten())
-    model.add(Dense(512))
+#    model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(nb_classes))
